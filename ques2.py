9578069cc550f1e646307287c927b9a26013bdab
vikas <hustlewithvikas09@gmail.com>
2026-02-08 03:26:14 +0530

    Check if a number is even or odd

---
+++ ques2.py
@@ -0,0 +1,9 @@
+# Check if a number is even or odd.
+
+a=int(input("enter the number to find it's odd or even  = "))
+
+if a%2==0:
+    print("this number this even : ")
+elif a%2!=0:
+    print("this number this not a odd number ")
+
