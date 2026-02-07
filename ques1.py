7b4a179eafe102d411ce1569c3492a2fd2020c92
vikas <hustlewithvikas09@gmail.com>
2026-02-08 03:18:49 +0530

    Added Day1 logic

---
+++ ques1.py
@@ -0,0 +1,12 @@
+# 1. Take a number and print whether it’s positive, negative, or zero.
+
+a=int(input("enter the number to find :"))
+
+if a<0:
+    print("this number is negative:")
+elif a>0:
+    print("this number is positive:")
+elif a==0:
+    print("this number this zero")
+
+    # end of the code 
